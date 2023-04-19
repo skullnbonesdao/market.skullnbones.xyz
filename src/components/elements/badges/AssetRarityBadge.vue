47aa56f4f7cf5dd08cfd5541b64cbac2a79d5391
dzwg <paule22@protonmail.com>
2023-04-19 22:58:06 +0200

    - updated css

--- src/components/elements/badges/AssetRarityBadge.vue
+++ src/components/elements/badges/AssetRarityBadge.vue
@@ -1,33 +1,33 @@
 <template>
-  <div>
+  <div class="">
     <span
       v-if="props.asset_class === Rarity.Uncommon"
-      class="rarity bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300"
       >Uncommon</span
     >
     <span
       v-if="props.asset_class === Rarity.Common"
-      class="rarity bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300"
       >Common</span
     >
     <span
       v-if="props.asset_class === Rarity.Rare"
-      class="rarity bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300"
       >Rare</span
     >
     <span
       v-if="props.asset_class === Rarity.Epic"
-      class="rarity bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300"
       >Epic</span
     >
     <span
       v-if="props.asset_class === Rarity.Legendary"
-      class="rarity bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300"
       >Legendary</span
     >
     <span
       v-if="props.asset_class === Rarity.Anomaly"
-      class="rarity bg-orange-100 text-orange-800 dark:bg-orange-900 dark:text-orange-300"
+      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded d uppercase bg-orange-100 text-orange-800 dark:bg-orange-900 dark:text-orange-300"
       >Anomaly</span
     >
   </div>
--- src/components/naviagtion/FooterComponent.vue
+++ src/components/naviagtion/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <div class="flex flex-row">
-      <div class="flex items-center px-2">RPC:</div>
+  <div class="space-y-2">
+    <div class="p-card flex flex-row p-2 space-x-2">
+      <div class="flex items-center">RPC:</div>
       <Dropdown
         v-model="selected_option"
         :options="drop_downOptions"
@@ -10,15 +10,18 @@
         class="w-full md:w-14rem"
       />
     </div>
-    <div class="flex w-full justify-center text-center">Support us:</div>
-    <div class="flex w-full text-sm justify-center text-center">
-      FULRbdyHJ7AzRCNVjA6NZoUD19EQ5a9swWdyaN4d9d7P
+    <div class="p-card p-2">
+      <div class="flex w-full justify-center text-center">Support us:</div>
+      <div class="flex w-full text-sm justify-center text-center">
+        FULRbdyHJ7AzRCNVjA6NZoUD19EQ5a9swWdyaN4d9d7P
+      </div>
     </div>
-
-    <div class="flex w-full justify-center text-center">
-      © [2022 S&B + HEIM] All Rights Reserved.
+    <div class="p-card p-2">
+      <div class="flex w-full justify-center text-center">
+        © [2022 S&B + HEIM] All Rights Reserved.
+      </div>
+      <div class="flex w-full justify-center">{{ version }}</div>
     </div>
-    <div class="flex w-full justify-center">{{ version }}</div>
   </div>
 </template>
 
