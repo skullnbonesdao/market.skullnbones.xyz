abd2e18b7db1d28ae12184bb43e722f5ddcbe8e8
dzwg <paule22@protonmail.com>
2023-04-21 13:03:14 +0200

    + updated theme dark

--- src/components/elements/tables/WalletNftsTable.vue
+++ src/components/elements/tables/WalletNftsTable.vue
@@ -14,12 +14,30 @@
 
       <Column field="metadata.mintAddress" header="Mint"></Column>
       <Column field="metadata.symbol" header="Symbol"></Column>
+
+      <Column header="Actions">
+        <template #body="slotProps"
+          ><div class="flex flex-row space-x-2">
+            <Button><i class="pi pi-send"></i></Button>
+
+            <Button><i class="pi pi-send pi-reply"></i></Button>
+
+            <Button v-if="!is_unsecured" @click="is_unsecured = true"
+              ><i class="pi pi-trash"></i
+            ></Button>
+            <Button v-else @click="is_unsecured = false"
+              ><i class="pi pi-exclamation-triangle"></i
+            ></Button>
+          </div>
+        </template>
+      </Column>
     </DataTable>
     <ProgressSpinner v-if="useGlobalStore().wallet.nfts.is_loading" />
   </div>
 </template>
 
 <script setup lang="ts">
+import Button from "primevue/button";
 import ProgressSpinner from "primevue/progressspinner";
 import { useGlobalStore } from "../../../stores/GlobalStore";
 import Skeleton from "primevue/skeleton";
@@ -27,6 +45,9 @@ import Image from "primevue/image";
 
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
+import { ref } from "vue";
+
+const is_unsecured = ref(false);
 
 function resolveUrl(url_json: string): string {
   let img_url = "";
