ca220a41750e46139a3d7870af5f32203f6c79a8
dzwg <paule22@protonmail.com>
2023-05-14 22:51:31 +0200

    + updated PortfolioView.vue2

--- src/components/elements/portfolio_elements/PortfolioAssetsTable.vue
+++ src/components/elements/portfolio_elements/PortfolioAssetsTable.vue
@@ -10,8 +10,23 @@ import { I_TokenData, useGlobalStore } from "../../../stores/GlobalStore";
 import { CURRENCIES } from "../../../static/currencies";
 import { ref } from "vue";
 import NoData from "../NoData.vue";
+import { E_EXPLORER, EXPLORER } from "../../../static/explorer";
+import ExplorerIcon from "../icons_images/ExplorerIcon.vue";
+import Button from "primevue/button";
+import { Connection, PublicKey, Transaction } from "@solana/web3.js";
+import * as spl from "@solana/spl-token";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useWallet } from "solana-wallets-vue";
+import { useToast } from "primevue/usetoast";
+import Toast from "primevue/toast";
 
 const expandedRows = ref([]);
+const is_unsecured = ref(false);
+const toast = useToast();
 
 const props = defineProps({
   option_l1: {
@@ -38,22 +53,73 @@ function filter_list(option_l1: String, option_l2?: string): I_TokenData[] {
   return useGlobalStore().wallet.tokens;
 }
 
-const onRowExpand = (event: any) => {
-  //toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.name, life: 3000 });
-};
-const onRowCollapse = (event: any) => {
-  //   toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.name, life: 3000 });
-};
+function btn_make_unsecure() {
+  is_unsecured.value = true;
+  toast.add({
+    severity: "warn",
+    summary: "Burning - active",
+    detail: "Click again to burn your NFT!",
+    life: 3000,
+  });
+}
+
+async function btn_action_burn(nft_mint_to_burn: string) {
+  const connection = new Connection(useGlobalStore().rpc.url);
+
+  const associatedAddress = await Token.getAssociatedTokenAddress(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    new PublicKey(nft_mint_to_burn),
+    new PublicKey(useWallet().publicKey.value ?? "")
+  );
+
+  const burn_instruction = spl.Token.createBurnInstruction(
+    TOKEN_PROGRAM_ID,
+    new PublicKey(nft_mint_to_burn),
+    associatedAddress,
+    new PublicKey(useWallet().publicKey.value ?? ""),
+    [],
+    1
+  );
+
+  const close_account_instruction = spl.Token.createCloseAccountInstruction(
+    TOKEN_PROGRAM_ID,
+    associatedAddress,
+    new PublicKey(useWallet().publicKey.value ?? ""),
+    new PublicKey(useWallet().publicKey.value ?? ""),
+    []
+  );
+
+  const instuctions = new Transaction().add(
+    burn_instruction,
+    close_account_instruction
+  );
+
+  const tx_signature = await useWallet().sendTransaction(
+    instuctions,
+    connection
+  );
+
+  const is_confirmed = await connection.confirmTransaction(
+    tx_signature,
+    "processed"
+  );
+
+  if (is_confirmed) {
+    console.log("Burned");
+  } else {
+    console.log("errror buring");
+  }
+}
 </script>
 
 <template>
   <div>
+    <Toast />
     <DataTable
       v-if="filter_list(option_l1, option_sa).length"
       v-model:expandedRows="expandedRows"
       :value="filter_list(option_l1, option_sa)"
-      @rowExpand="onRowExpand"
-      @rowCollapse="onRowCollapse"
       tableStyle="min-width: 50rem"
     >
       <Column expander style="width: 5rem" />
@@ -120,18 +186,21 @@ const onRowCollapse = (event: any) => {
                       preview
                     />
                   </div>
-                  <div class="grid grid-cols-2 text-sm">
-                    <div class="grid grid-cols-1">
+                  <div class="flex flex-row items-center space-x-2">
+                    <div class="flex flex-col">
                       <div>Name:</div>
                       <div>Symbol:</div>
                       <div>Mint:</div>
                       <div>Account:</div>
                       <div>Amount:</div>
                     </div>
-                    <div class="grid grid-cols-1">
+
+                    <div class="flex flex-col">
                       <div>{{ slotProps.data.account_metadata.name }}</div>
                       <div>{{ slotProps.data.account_metadata.symbol }}</div>
-                      <div>{{ slotProps.data.token_mint }}</div>
+                      <div class="grid grid-cols-2">
+                        {{ slotProps.data.token_mint }}
+                      </div>
                       <div>{{ slotProps.data.token_account }}</div>
                       <div>
                         x{{
@@ -140,13 +209,84 @@ const onRowCollapse = (event: any) => {
                         }}
                       </div>
                     </div>
+                    <div class="flex flex-col space-y-2">
+                      <div class="text-transparent">-</div>
+                      <div class="text-transparent">-</div>
+                      <div class="flex flex-row space-x-1">
+                        <ExplorerIcon
+                          class="w-5"
+                          :explorer="
+                            EXPLORER.find((e) => e.type === E_EXPLORER.SOLSCAN)
+                          "
+                          :address="slotProps.data.token_mint"
+                        />
+                        <ExplorerIcon
+                          class="w-5"
+                          :explorer="
+                            EXPLORER.find((e) => e.type === E_EXPLORER.SOLANAFM)
+                          "
+                          :address="slotProps.data.token_mint"
+                        />
+                        <ExplorerIcon
+                          class="w-5"
+                          :explorer="
+                            EXPLORER.find(
+                              (e) => e.type === E_EXPLORER.STARATLAS
+                            )
+                          "
+                          :address="slotProps.data.token_mint"
+                        />
+                      </div>
+                      <div class="flex flex-row space-x-1">
+                        <ExplorerIcon
+                          class="w-5"
+                          :explorer="
+                            EXPLORER.find((e) => e.type === E_EXPLORER.SOLSCAN)
+                          "
+                          :address="slotProps.data.token_account"
+                        />
+                        <ExplorerIcon
+                          class="w-5"
+                          :explorer="
+                            EXPLORER.find((e) => e.type === E_EXPLORER.SOLANAFM)
+                          "
+                          :address="slotProps.data.token_account"
+                        />
+                      </div>
+
+                      <div class="text-transparent">-</div>
+                    </div>
                   </div>
 
                   <div class="flex w-full justify-end">
                     <div class="flex flex-row space-x-2">
-                      <Button>123</Button>
-                      <Button>123</Button>
-                      <Button>123</Button>
+                      <Button
+                        v-tooltip.bottom="'Burn NFT (unlock)'"
+                        v-if="!is_unsecured"
+                        class="p-button-secondary"
+                        @click="btn_make_unsecure"
+                        ><i class="pi pi-trash"></i
+                      ></Button>
+                      <Button
+                        v-else
+                        @click="
+                          btn_action_burn(slotProps.data.token_mint).then(
+                            () => {
+                              is_unsecured = false;
+                            }
+                          )
+                        "
+                        v-tooltip.bottom="'Burn NFT (unlocked)'"
+                        class="p-button-danger"
+                        ><i class="pi pi-exclamation-triangle"></i
+                      ></Button>
+
+                      <Button
+                        disabled
+                        v-tooltip.bottom="'Send NFT'"
+                        class="p-button-secondary"
+                        ><i class="pi pi-send"></i
+                      ></Button>
                     </div>
                   </div>
                 </div>
