30b9f58f33f129a3d2cd4eaed45580e5a908e9b2
dzwg <paule22@protonmail.com>
2023-06-18 22:32:20 +0200

    + updated OrderBook.vue moved to client form service

--- src/components/elements/marketplace_elements/OrderBook.vue
+++ src/components/elements/marketplace_elements/OrderBook.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col text-sm lg:text-base" style="min-width: 450px">
     <ProgressSpinner
-      v-if="!buy_order.length || !sell_order.length"
+      v-if="!buy_orders.length || !sell_orders.length"
       class="flex justify-center"
     />
 
@@ -10,22 +10,22 @@
         <order-book-header></order-book-header>
         <order-book-header :reverse_order="true"></order-book-header>
         <div>
-          <div v-for="orderBlock in buy_order">
+          <div v-for="orderBlock in buy_orders">
             <order-book-row
               class=""
               :order="orderBlock"
               side="buy"
-              :max_size="Math.max(...buy_order.map((o) => o.size))"
+              :max_size="Math.max(...buy_orders.map((o) => o.size))"
             />
           </div>
         </div>
         <div>
-          <div v-for="orderBlock in sell_order">
+          <div v-for="orderBlock in sell_orders">
             <order-book-row
               class=""
               :order="orderBlock"
               side="sell"
-              :max_size="Math.max(...sell_order.map((o) => o.size))"
+              :max_size="Math.max(...sell_orders.map((o) => o.size))"
               :reverse_order="true"
             />
           </div>
@@ -37,7 +37,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { useGlobalStore } from "../../../stores/GlobalStore";
 import OrderBookHeader from "./OrderBookHeader.vue";
 import OrderBookRow from "./OrderBookRow.vue";
@@ -46,54 +46,96 @@ import {
   useStaratlasGmStore,
 } from "../../../stores/StaratlasGmStore";
 import ProgressSpinner from "primevue/progressspinner";
+import { GM_PROGRAM_ID } from "../../../static/constants/StarAtlasConstants";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { OrderSide } from "@staratlas/factory";
 
 const is_loading = ref(true);
 
 const selectedCurrency = ref();
 
-onMounted(async () => {
-  await useStaratlasGmStore().init();
-});
+const buy_orders = ref<OrderBookOrderMap[]>([]);
+const sell_orders = ref<OrderBookOrderMap[]>([]);
+watchEffect(async () => {
+  let orders =
+    await useStaratlasGmStore().gmClientService.getOpenOrdersForAsset(
+      new Connection(useGlobalStore().rpc.url),
+      useGlobalStore().symbol.mint_asset,
+      new PublicKey(GM_PROGRAM_ID)
+    );
 
-const buy_order = computed(() => {
-  let orderbook_map: OrderBookOrderMap[] = [];
-
-  useStaratlasGmStore()
-    .gmClientService.getSellOrdersByCurrencyAndItem(
-      useGlobalStore().symbol.mint_pair.toString(),
-      useGlobalStore().symbol.mint_asset.toString()
+  buy_orders.value = orders
+    .filter(
+      (order) =>
+        order.currencyMint === useGlobalStore().symbol.mint_pair.toString()
     )
-    .forEach((order) => {
-      orderbook_map.push({
+    .filter((order) => order.orderType === OrderSide.Buy)
+    .map((order) => {
+      return {
         price: order.uiPrice,
         size: order.orderQtyRemaining,
         owners: [order.owner],
-      });
-    });
+      };
+    })
+    .sort((a, b) => a.size - b.price)
+    .reverse() as [];
 
-  orderbook_map.sort((a, b) => a.price - b.price).reverse();
-
-  return orderbook_map;
-});
-
-const sell_order = computed(() => {
-  let orderbook_map: OrderBookOrderMap[] = [];
-
-  useStaratlasGmStore()
-    .gmClientService.getSellOrdersByCurrencyAndItem(
-      useGlobalStore().symbol.mint_pair.toString(),
-      useGlobalStore().symbol.mint_asset.toString()
+  sell_orders.value = orders
+    .filter(
+      (order) =>
+        order.currencyMint === useGlobalStore().symbol.mint_pair.toString()
     )
-    .forEach((order) => {
-      orderbook_map.push({
+    .filter((order) => order.orderType === OrderSide.Sell)
+    .map((order) => {
+      return {
         price: order.uiPrice,
         size: order.orderQtyRemaining,
         owners: [order.owner],
-      });
-    });
+      };
+    })
 
-  orderbook_map.sort((a, b) => a.price - b.price);
-
-  return orderbook_map;
+    .sort((a, b) => a.size - b.price) as [];
 });
+
+// const buy_order = computed(() => {
+//   let orderbook_map: OrderBookOrderMap[] = [];
+//
+//   useStaratlasGmStore()
+//     .gmClientService.getOpenOrdersForAsset(
+//       useGlobalStore().symbol.mint_pair.toString(),
+//       useGlobalStore().symbol.mint_asset.toString()
+//     )
+//     .forEach((order) => {
+//       orderbook_map.push({
+//         price: order.uiPrice,
+//         size: order.orderQtyRemaining,
+//         owners: [order.owner],
+//       });
+//     });
+//
+//   orderbook_map.sort((a, b) => a.price - b.price).reverse();
+//
+//   return orderbook_map;
+// });
+
+// const sell_order = computed(() => {
+//   let orderbook_map: OrderBookOrderMap[] = [];
+//
+//   useStaratlasGmStore()
+//     .gmClientService.getSellOrdersByCurrencyAndItem(
+//       useGlobalStore().symbol.mint_pair.toString(),
+//       useGlobalStore().symbol.mint_asset.toString()
+//     )
+//     .forEach((order) => {
+//       orderbook_map.push({
+//         price: order.uiPrice,
+//         size: order.orderQtyRemaining,
+//         owners: [order.owner],
+//       });
+//     });
+//
+//   orderbook_map.sort((a, b) => a.price - b.price);
+//
+//   return orderbook_map;
+// });
 </script>
