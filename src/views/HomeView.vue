987820f73d0bf55ad5c51f7ff2f7c74b7cf4707f
dzwg <paule22@protonmail.com>
2023-06-23 18:38:41 +0200

    + updated landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="background">
-    <section class="pt-20">
-      <div
-        class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
-      >
-        <div class="place-self-center lg:col-span-7 rounded-lg">
-          <h1
-            class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-gray-100 dark:text-gray-300"
-          >
-            RogueMarket
-          </h1>
-          <div
-            class="max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100 dark:text-gray-300 animate-fade-in-down animate-count-1 animate-duration-2s fw100"
-          >
-            <image src="/images/sa-powered_by_the_people.png"></image>
-          </div>
-
-          <!--          <p
-            class="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-xl text-gray-100 dark:text-gray-300"
-          >
-            by S&B
-          </p>-->
-        </div>
+  <div class="h-screen">
+    <div class="flex justify-center background masked-overflow">
+      <section class="pt-20">
         <div
-          class="hidden lg:mt-0 lg:col-span-5 lg:flex animate-pulse animate-duration-5s shadow-2xl"
+          class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
         >
-          <img alt="mockup" src="/images/rouge_market_background_1.webp" />
+          <div class="place-self-center lg:col-span-7 rounded-lg">
+            <h1
+              class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-gray-100 dark:text-gray-300"
+            >
+              RogueMarket
+            </h1>
+            <div
+              class="max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100 dark:text-gray-300 animate-fade-in-down animate-count-1 animate-duration-2s fw100"
+            >
+              <image src="/images/sa-powered_by_the_people.png"></image>
+            </div>
+
+            <!--          <p
+              class="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-xl text-gray-100 dark:text-gray-300"
+            >
+              by S&B
+            </p>-->
+          </div>
+          <div
+            class="hidden lg:mt-0 lg:col-span-5 lg:flex animate-pulse animate-duration-5s shadow-2xl"
+          >
+            <img alt="mockup" src="/images/rouge_market_background_1.webp" />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -39,5 +41,13 @@
   background-image: url("../assets/rouge_market_background_1.webp");
   background-repeat: no-repeat, repeat;
   background-size: cover;
+}
+
+.masked-overflow {
+  margin: 5px 0px;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0 80px 50px #141414;
+  mask-repeat: no-repeat, no-repeat;
 }
 </style>
