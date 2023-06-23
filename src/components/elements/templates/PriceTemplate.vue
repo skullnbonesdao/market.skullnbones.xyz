15d914aa3333e0ec7f02a6242c6a988c6a806663
dzwg <paule22@protonmail.com>
2023-06-23 15:22:07 +0200

    + migrated styling to ships table

--- src/components/elements/templates/PriceTemplate.vue
+++ src/components/elements/templates/PriceTemplate.vue
@@ -1,25 +1,29 @@
 <template>
   <div
-    class="flex flex-row gap-2 p-2 rounded-lg items-center border-2 p-card"
     :class="useGlobalStore().is_dark ? 'border-gray-900' : ''"
+    class="flex flex-row gap-2 p-2 rounded-lg items-center border-2 p-card"
   >
     <div class="flex flex-col space-y-1 items-center">
       <CurrencyIcon
         v-if="props.currency"
-        style="width: 20px"
         :currency="CURRENCIES.find((c) => c.type === E_CURRENCIES.USDC)"
+        style="width: 20px"
       />
       <CurrencyIcon
         v-if="props.currency"
-        style="width: 20px"
         :currency="CURRENCIES.find((c) => c.type === E_CURRENCIES.ATLAS)"
+        style="width: 20px"
       />
+    </div>
+    <div class="flex flex-col gap-2">
+      <div class="border h-1rem"></div>
+      <div class="border h-1rem"></div>
     </div>
     <div class="flex flex-col w-24">
       <p
         v-if="props.currency?.type === E_CURRENCIES.USDC"
-        class="flex justify-end"
         :class="props.currency.type === E_CURRENCIES.USDC ? 'underline' : ''"
+        class="flex justify-end"
       >
         {{ format_price_reasonable(props.price) }}
       </p>
@@ -34,18 +38,18 @@
       </p>
 
       <p
-        class="flex justify-end text-sm"
         v-if="props.currency?.type === E_CURRENCIES.ATLAS"
         :class="props.currency?.type === E_CURRENCIES.ATLAS ? 'underline' : ''"
+        class="flex justify-end text-sm"
       >
         {{ format_price_reasonable(props.price) }}
       </p>
-      <p class="flex justify-end text-sm" v-else>-</p>
+      <p v-else class="flex justify-end text-sm">-</p>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { PropType } from "vue";
 import {
   CURRENCIES,
--- src/components/elements/templates/VwapTemplate.vue
+++ src/components/elements/templates/VwapTemplate.vue
@@ -1,19 +1,20 @@
 <template>
   <div
-    class="flex flex-row items-center p-2 rounded-lg border-2 p-card"
     :class="useGlobalStore().is_dark ? 'border-gray-900' : ''"
+    class="flex flex-row items-center gap-2 p-inputtext"
   >
     <CurrencyIcon
-      style="width: 24px"
       :currency="CURRENCIES.find((c) => c.type === E_CURRENCIES.USDC)"
+      style="width: 24px"
     />
+    <div class="border h-1rem"></div>
     <p class="flex w-full justify-end">
       {{ pros.vwap }}
     </p>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import CurrencyIcon from "../../icon-helper/CurrencyIcon.vue";
 import { CURRENCIES, E_CURRENCIES } from "../../../static/currencies";
 import { useGlobalStore } from "../../../stores/GlobalStore";
